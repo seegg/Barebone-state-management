e2271292979d7490bced55ee502df93e66e98be4
yuan tan <dyxtan@gmail.com>
2023-08-24 00:37:52 +1200

    add tests for actions

--- test/createStore.test.ts
+++ test/createStore.test.ts
@@ -5,26 +5,30 @@ import { renderHook, act } from '@testing-library/react';
 import { createStore } from '../src/barebone';
 
 describe('createStore()', () => {
+  // Default values for setting up the tests.
   interface TestCounter {
     value: number;
     isUpdating: boolean;
   }
 
+  const initialState: TestCounter = {
+    value: 0,
+    isUpdating: false,
+  };
+
+  const name = 'test';
+
   // Test the useStore hook.
   describe('useStore()', () => {
-    const initialState: TestCounter = {
-      value: 0,
-      isUpdating: false,
-    };
+    // Create the store for all 'useStore' tests.
     const { useStore, actions, store } = createStore({
-      name: 'test',
+      name,
       initialState,
       actions: {
         increment: (state) => ({ ...state, value: state.value + 1 }),
         setCounterValue: (state, value: number) => ({ ...state, value }),
       },
     });
-
     it('Selects the correct properties from the store', () => {
       /** Value from the store. */
       const expected = store.test.value;
@@ -72,6 +76,57 @@ describe('createStore()', () => {
       // Set a value to passes the check.
       act(() => actions.setCounterValue(15));
       expect(result.current).toEqual(store.test.value);
+    });
+  });
+
+  // Test synchronous actions
+  describe('sync actions', () => {
+    it('Should update the store when called', () => {
+      const { actions, store } = createStore({
+        name,
+        initialState,
+        actions: {
+          increment: (state) => ({ ...state, value: state.value + 1 }),
+          setCounterValue: (state, value: number) => ({ ...state, value }),
+        },
+      });
+
+      const oldValue = store.test.value;
+      actions.increment();
+      expect(store.test.value).toEqual(oldValue + 1);
+
+      const valueToBeSet = store.test.value + 100000;
+      actions.setCounterValue(valueToBeSet);
+      expect(store.test.value).toEqual(valueToBeSet);
+    });
+
+    it('Triggers updates on all hooks connected to the store', () => {
+      const { useStore, actions, store } = createStore({
+        name,
+        initialState,
+        actions: {
+          increment: (state) => ({ ...state, value: state.value + 1 }),
+          setCounterValue: (state, value: number) => ({ ...state, value }),
+        },
+      });
+
+      const oldValue = store.test.value;
+
+      const hook1 = renderHook(() => useStore((store) => store));
+      const hook2 = renderHook(() => useStore((store) => store));
+      const hook3 = renderHook(() => useStore((store) => store));
+
+      [hook1, hook2, hook3].forEach(({ result: { current } }) => {
+        expect(current.test.value).toEqual(oldValue);
+      });
+
+      const newValue = oldValue + 10;
+      act(() => {
+        actions.setCounterValue(newValue);
+      });
+      [hook1, hook2, hook3].forEach(({ result: { current } }) => {
+        expect(current.test.value).toEqual(newValue);
+      });
     });
   });
 });
